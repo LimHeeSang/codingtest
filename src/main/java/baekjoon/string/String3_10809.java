5c51ddc8f826f9e0ecbf3862059af4907d38ea6c
heesang <dlagmltkd12@naver.com>
2021-11-22 03:29:35 +0900

    Feat : 문자열3, 백준 10809번

---
+++ src/main/java/baekjoon/string/String3_10809.java
@@ -0,0 +1,22 @@
+package baekjoon.string;
+import java.io.BufferedReader;
+        import java.io.BufferedWriter;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.io.OutputStreamWriter;
+
+public class String3_10809 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        for(char ch = 'a'; ch<='z'; ch++) {
+            bw.write(str.indexOf(ch) +" ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
