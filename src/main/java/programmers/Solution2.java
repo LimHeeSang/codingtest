5bf0c6b93240dce97e9d1d3f2d6d37534b784c8d
heesang <dlagmltkd12@naver.com>
2022-01-01 23:32:30 +0900

    Feat : level1-2, 프로그래머스 신규 아이디 추천

---
+++ src/main/java/programmers/Solution2.java
@@ -0,0 +1,29 @@
+package programmers;
+
+//level1-2 신규 아이디 추천
+class Solution2 {
+    public String solution(String new_id) {
+        String id = new_id.toLowerCase(); // 1단계
+
+        id = id.replaceAll("[^-_.a-z0-9]", ""); // 2단계
+        id = id.replaceAll("[.]{2,}", "."); // 3단계
+        id = id.replaceAll("^[.]|[.]$", "");    // 4단계
+
+        if (id.equals("")) {    // 5단계
+            id += "a";
+        }
+
+        if (id.length() >= 16) {     // 6단계
+            id = id.substring(0, 15);
+            id = id.replaceAll("[.]$","");
+        }
+
+        if (id.length() <= 2) {  // 7단계
+            while (id.length() < 3) {
+                id += id.charAt(id.length()-1);
+            }
+        }
+
+        return id;
+    }
+}
