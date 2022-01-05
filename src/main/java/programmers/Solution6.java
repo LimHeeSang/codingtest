af9ab0191a8cee573d463207efdfd1142e011f3e
heesang <dlagmltkd12@naver.com>
2022-01-05 18:05:42 +0900

    Feat : level1-6, 프로그래머스 없는 숫자 더하기

---
+++ src/main/java/programmers/Solution6.java
@@ -0,0 +1,23 @@
+package programmers;
+
+//level1-6 없는 숫자 더하기
+class Solution6 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        boolean[] check = new boolean[10];
+
+        for(int number:numbers){
+            check[number] = true;
+        }
+
+        for(int i=0; i<check.length; i++){
+            if(check[i] == false){
+                answer += i;
+            }
+        }
+
+
+        return answer;
+    }
+}
