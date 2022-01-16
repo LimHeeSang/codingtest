82af73c383e8e231088df4f782619e359d088e9b
heesang <dlagmltkd12@naver.com>
2022-01-17 05:09:40 +0900

    Feat : level1-18, 프로그래머스 예산

---
+++ src/main/java/programmers/Solution18.java
@@ -0,0 +1,21 @@
+package programmers;
+import java.util.*;
+
+//level1-18 예산
+class Solution18 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            if(d[i] <= budget){
+                budget -= d[i];
+                answer++;
+            }else{
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
