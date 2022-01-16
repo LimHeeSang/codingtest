66164262be40d384d4486ff2a4d42748536d22db
heesang <dlagmltkd12@naver.com>
2022-01-17 05:20:34 +0900

    Refactor : level1-20, 프로그래머스 2016년

--- src/main/java/programmers/Solution20.java
+++ src/main/java/programmers/Solution20.java
@@ -3,19 +3,15 @@ package programmers;
 //level1-20 2016년
 class Solution20 {
     public String solution(int month, int date) {
-        String answer = "";
 
         String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
         int sum = 0;
 
-        if(month == 1){
-            sum = date-1;
-        }else{
-            for(int i=month-1; i>0; i--){
-                sum += dateByMonth(i);
-            }
-            sum += date-1;
-        }
+
+        for(int i=month-1; i>0; i--)
+            sum += dateByMonth(i);
+
+        sum += date-1;
 
         return day[sum%7];
     }
@@ -27,4 +23,5 @@ class Solution20 {
 
         return dates[month-1];
     }
+
 }
