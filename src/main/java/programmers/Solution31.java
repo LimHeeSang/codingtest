b0e9950572b3cc061dc0be29a9e0e326ddf2dbd7
heesang <dlagmltkd12@naver.com>
2022-01-24 03:11:47 +0900

    Feat : level1-31, 프로그래머스 문자열 내 p와 y의 개수

---
+++ src/main/java/programmers/Solution31.java
@@ -0,0 +1,21 @@
+package programmers;
+
+//level1-31 문자열 내 p와 y의 개수
+class Solution31 {
+    boolean solution(String s) {
+
+        s = s.toLowerCase();
+
+        int countP = 0;
+        int countY = 0;
+
+        char[] ch = s.toCharArray();
+        for(char c : ch){
+            if(c == 'p') countP++;
+            if(c == 'y') countY++;
+        }
+
+        if(countP == countY) return true;
+        else return false;
+    }
+}
