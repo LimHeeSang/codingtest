61960d6f665fc0e1e088261050bb435b402817fd
heesang <dlagmltkd12@naver.com>
2022-01-26 23:45:58 +0900

    Feat : level1-37, 프로그래머스 문자열을 정수로 바꾸기

---
+++ src/main/java/programmers/Solution37.java
@@ -0,0 +1,12 @@
+package programmers;
+
+//level1-37 문자열을 정수로 바꾸기
+class Solution37 {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
