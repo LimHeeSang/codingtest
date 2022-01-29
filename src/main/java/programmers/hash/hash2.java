6c97140a33cc40899d621093031b9ca427afbf1f
heesang <dlagmltkd12@naver.com>
2022-01-30 03:59:08 +0900

    Feat : hash2 프로그래머스 전화번호 목록

---
+++ src/main/java/programmers/hash/hash2.java
@@ -0,0 +1,20 @@
+package programmers.hash;
+import java.util.*;
+
+//hash2 전화번호 목록
+class hash2 {
+    public boolean solution(String[] pb) {
+        boolean answer = true;
+
+        Arrays.sort(pb);
+
+        for(int i=0; i<pb.length-1; i++){
+            if(pb[i+1].startsWith(pb[i])){
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
