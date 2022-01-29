e74c836d71eb5582d27fcba7fbd5819513c96f27
heesang <dlagmltkd12@naver.com>
2022-01-30 01:56:40 +0900

    Feat : level1-54 프로그래머스 직사각형 별찍기

---
+++ src/main/java/programmers/level1/Solution54.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+import java.util.Scanner;
+
+//level1-54 직사각형 별찍기
+class Solution54 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i=0; i<b; i++){
+            for(int j=0; j<a; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
