2a75bf6df9f3b275a24da0c6e75d8f375d8daaea
heesang <dlagmltkd12@naver.com>
2022-01-30 03:58:52 +0900

    Feat : hash1 프로그래머스 완주하지 못한 선수

---
+++ src/main/java/programmers/hash/hash1.java
@@ -0,0 +1,26 @@
+package programmers.hash;
+import java.util.*;
+
+//hash1 완주하지 못한 선수
+class hash1 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Map<String, Integer> map = new HashMap<>();
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0)+1);
+        }
+
+        for(String c : completion){
+            map.put(c, map.get(c)-1);
+        }
+
+        Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+        for(Map.Entry<String, Integer> entry : entrySet){
+            if(entry.getValue() != 0) answer = entry.getKey();
+        }
+
+
+        return answer;
+    }
+}
