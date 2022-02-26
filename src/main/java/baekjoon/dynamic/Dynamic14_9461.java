4a46d1d4ad1b8c438b3f55860a74833e96d98372
heesang <dlagmltkd12@naver.com>
2022-02-26 23:18:01 +0900

    Feat : 다이나믹14, 백준 9461번

---
+++ src/main/java/baekjoon/dynamic/Dynamic14_9461.java
@@ -0,0 +1,13 @@
+package baekjoon.dynamic;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Dynamic14_9461 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
