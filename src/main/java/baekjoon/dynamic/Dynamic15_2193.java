30498b903ba6ffd528d08add0ff10797877ac7a7
heesang <dlagmltkd12@naver.com>
2022-02-27 23:55:26 +0900

    Feat : 다이나믹15, 백준 2193번

---
+++ src/main/java/baekjoon/dynamic/Dynamic15_2193.java
@@ -0,0 +1,38 @@
+package baekjoon.dynamic;
+
+import java.io.*;
+
+public class Dynamic15_2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[2][n];
+
+        if (n >= 1) {
+            dp[0][0] = 0;
+            dp[1][0] = 1;
+        }
+        if (n >= 2) {
+            dp[1][0] = 1;
+            dp[1][1] = 0;
+        }
+
+        for (int i = 2; i < n; i++) {
+            dp[0][i] = dp[0][i-1] + 2;
+            dp[1][i] = dp[1][i-1] + 1;
+        }
+
+        int answer;
+        if (n == 1 || n == 2) {
+            answer = 1;
+        }else {
+            answer = (dp[0][n - 1] + dp[1][n - 1]) - (dp[0][n - 2] + dp[1][n - 2]);
+        }
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+    }
+}
