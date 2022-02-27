3c55307120f1532213fd3d120c18e8e21a62b729
heesang <dlagmltkd12@naver.com>
2022-02-28 02:38:18 +0900

    Refactor : 다이나믹14, 백준 9461번

--- src/main/java/baekjoon/dynamic/Dynamic14_9461.java
+++ src/main/java/baekjoon/dynamic/Dynamic14_9461.java
@@ -1,13 +1,31 @@
 package baekjoon.dynamic;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Dynamic14_9461 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        long[] dp = new long[100];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 2;
+
+        for (int i = 5; i < 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        int m;
+        for (int i = 0; i < t; i++) {
+            m = Integer.parseInt(br.readLine());
+            bw.write(dp[m-1] + "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
