5082a05dd87711ebd742750966f1f2636ab7166e
heesang <dlagmltkd12@naver.com>
2022-03-01 23:06:45 +0900

    feat : 깊이우선탐색5, 백준 2583번

---
+++ src/main/java/baekjoon/dfs/Dfs5_2583.java
@@ -0,0 +1,13 @@
+package baekjoon.dfs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Dfs5_2583 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
