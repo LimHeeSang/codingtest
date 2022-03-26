41a1909f42722e5c583e1e032c6650e86377912f
heesang <dlagmltkd12@naver.com>
2022-03-26 22:56:49 +0900

    feat : 투포인터1, 백준 1806번

---
+++ src/main/java/baekjoon/twopointer/TwoPointer1_1806.java
@@ -0,0 +1,13 @@
+package baekjoon.twopointer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class TwoPointer1_1806 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
