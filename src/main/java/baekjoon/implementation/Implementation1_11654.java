f3bd73ae5c08b8d2c6595ce26d7f2c1dbb0e081e
heesang <dlagmltkd12@naver.com>
2022-04-12 12:34:56 +0900

    feat : 구현1, 백준 11654번

---
+++ src/main/java/baekjoon/implementation/Implementation1_11654.java
@@ -0,0 +1,14 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Implementation1_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int answer = br.readLine().charAt(0);
+        System.out.println(answer);
+    }
+}
