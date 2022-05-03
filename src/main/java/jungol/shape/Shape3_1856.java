493feca2efd14fe65ea6ec931e86830a3251c8fe
heesang <dlagmltkd12@naver.com>
2022-05-03 21:43:12 +0900

    feat : 도형3 정올 1856

---
+++ src/main/java/jungol/shape/Shape3_1856.java
@@ -0,0 +1,33 @@
+package jungol.shape;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Shape3_1856 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] result = new int[n][m];
+
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i % 2 == 0) {
+                    result[i][j] = count++;
+                } else {
+                    result[i][m - 1- j] = count++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
