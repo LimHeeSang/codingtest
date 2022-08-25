ff1451c5a8b45b55e4cc38377efa7477162eff27
heesang <dlagmltkd12@naver.com>
2022-08-25 22:46:56 +0900

    feat : 도형8 정올 1338

---
+++ src/main/java/jungol/shape/Shape8_1338.java
@@ -0,0 +1,36 @@
+package jungol.shape;
+
+import java.io.*;
+
+public class Shape8_1338 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        char[][] result = new char[n][n];
+
+        char alphabet = 'A';
+        for (int i = 0; i < n; i++) {
+            for (int j = i, k = n - 1; j < n; j++, k--) {
+                result[j][k] = alphabet++;
+                if (alphabet > 'Z') {
+                    alphabet = 'A';
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (result[i][j] == 0) {
+                    bw.write(" " + " ");
+                } else {
+                    bw.write(result[i][j] + " ");
+                }
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
