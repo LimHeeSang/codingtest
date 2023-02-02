7ef1b03d35de8c741a029b0c5fd03dab15bf0cbe
heesang <dlagmltkd12@naver.com>
2023-02-03 01:13:25 +0900

    feat: 노트정리 유니온 파인드, 무방향 그래프에서의 싸이클 찾는 알고리즘 추가

--- src/main/java/note/graph/UnionFind.java
+++ src/main/java/note/graph/UnionFind.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class UnionFind {
 
     static int[] parent;
+    static int cycleCount = 0;
 
     public static void main(String[] args) {
         int n = 9;  //노드는 1~9까지 존재
@@ -15,13 +16,14 @@ public class UnionFind {
 
         //간선 정보가 주어진다.
         // 노드다 한 집합인지 체크 여부는 순서에 영향을 받을 수 있으므로 parent 배열을 확인하는 것이 아니라, findParent 메소드를 사용하자.
-        int[][] lines = {{8, 9}, {1, 2}, {2, 3}, {3, 5}, {4, 6}, {6, 7}};
+        int[][] lines = {{8, 9}, {1, 2}, {2, 3} , {7, 4}, {5, 1}, {3, 5}, {4, 6}, {6, 7}};
 
         for (int[] line : lines) {
             unionParent(line[0], line[1]);
         }
 
-        System.out.println(Arrays.toString(parent));
+        // 무방향 그래프에서 유니온 파인드로 싸이클 찾기
+        System.out.println(cycleCount);
     }
 
     static int findParent(int node) {
@@ -36,8 +38,10 @@ public class UnionFind {
         node2 = findParent(node2);
         if (node1 > node2) {
             parent[node1] = node2;
-        } else {
+        } else if (node2 > node1) {
             parent[node2] = node1;
+        } else {    // 부모가 같다면 싸이클이 발생
+            cycleCount++;
         }
     }
 }
