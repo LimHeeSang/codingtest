ca4c6c695fa3343b29ed70661c776e3afb595648
LimHeeSang <dlagmltkd12@naver.com>
2023-02-13 02:02:45 +0900

    feat: 정렬7 백준 11651번

---
+++ src/main/java/baekjoon/sort/Sort7_11651.java
@@ -0,0 +1,52 @@
+package baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Sort7_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        List<Point> list = new LinkedList<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(list);
+
+        for (Point p : list) {
+            bw.write(p.x + " " + p.y + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    static class Point implements Comparable<Point>{
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            if (this.y == p.y) {
+                return this.x - p.x;
+            }
+            return this.y - p.y;
+        }
+    }
+}
